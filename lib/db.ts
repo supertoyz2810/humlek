6c72b5e28edd99e2f20c3f8cc2cd60807e24b190
HaoChungNhat <chungnhathao1997@gmail.com>
2024-10-21 17:29:42 +0700

    21102024_dev_hao_v1

---
+++ lib/db.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connect = async () => {
+    const connectionState = mongoose.connection.readyState;
+
+    if(connectionState === 1){
+        console.log("Already connected");
+        return;
+    }
+
+    if(connectionState === 2){
+        console.log("Connecting...");
+        return;
+    }
+
+    try {
+        mongoose.connect(MONGODB_URI!, {
+            dbName: 'nextrestapi',
+            bufferCommands: true,
+        });
+        console.log("connected");
+    } catch (error: any) {
+        console.log("Error: ", error);
+        throw new Error("Error: ", error);
+    }
+};
+export default connect;
