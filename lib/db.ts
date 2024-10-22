3f0befdfa518374288290a4ba2f854bc3e2c8c75
HaoChungNhat <chungnhathao1997@gmail.com>
2024-10-22 09:58:06 +0700

    22102024_dev_hao_v3

--- lib/db.ts
+++ lib/db.ts
@@ -21,7 +21,6 @@ const connect = async () => {
             bufferCommands: true
         });
         console.log("connected");
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
         console.log("Error: ", error);
         throw new Error("Error: ", error);
